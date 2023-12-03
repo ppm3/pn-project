c651a48c900ec651fe07348de3427fd2011467e8
ppm3 <mahatma.marroquin@gmail.com>
2023-12-03 16:24:13 -0600

    Fix (frontend): replace all frontend structure and libraries. Close #4

--- routes/web.php
+++ routes/web.php
@@ -1,36 +1,38 @@
 <?php
 
-use App\Http\Controllers\Admin\DashboardController;
-use App\Http\Controllers\Admin\LoginController;
+use App\Http\Controllers\ProfileController;
+use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
+use Inertia\Inertia;
 
 /*
 |--------------------------------------------------------------------------
 | Web Routes
 |--------------------------------------------------------------------------
 |
-| Here is where you can register web routes for your application.
-| These routes are loaded by the RouteServiceProvider and assigned to the "web" middleware group.
-| Make something great!
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
 |
 */
 
-// web segment
 Route::get('/', function () {
-    return view('welcome');
+    return Inertia::render('Welcome', [
+        'canLogin' => Route::has('login'),
+        'canRegister' => Route::has('register'),
+        'laravelVersion' => Application::VERSION,
+        'phpVersion' => PHP_VERSION,
+    ]);
 });
 
-// admin segment
-Route::prefix('admin')->group(function () {
-    Route::get('/' , [LoginController::class, 'showLoginForm'])->name('admin.login');
-    Route::post('/login', [LoginController::class, 'login'])->name('admin.login.submit');
-    
-    Route::middleware('auth')->group(function () {
-        Route::get('/logout', [LoginController::class, 'logout'])->name('admin.logout');
-        Route::get('/dashboard', [DashboardController::class, ''])->name('admin.dashboard');
-    });
+Route::get('/dashboard', function () {
+    return Inertia::render('Dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
-
-
-
+require __DIR__.'/auth.php';
